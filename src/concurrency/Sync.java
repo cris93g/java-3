7691a3ec2468bef1f352fc21ab1963698ff07319
cris93g <37388115+cris93g@users.noreply.github.com>
2020-07-06 04:36:34 -0500

    pushing java-3

--- src/concurrency/Sync.java
+++ src/concurrency/Sync.java
@@ -6,28 +6,29 @@ import java.util.Random;
 
 class Sync extends Thread {
     List nums = new ArrayList<>();
-
     Sync(String name) {
         super(name);
     }
 
-    @Override 
+    @Override
     public void run() {
         Random rand = new Random();
-
-        for(int i=0; i<100; i++) {
+        int i=0;
+        while(i<100){
             int randInt = rand.nextInt(100);
             this.nums.add(randInt);
+            i++;
         }
+
     }
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) throws InterruptedException {
         Sync syncThread = new Sync("sync thread");
         syncThread.start();
-
+        syncThread.join();
         System.out.println(syncThread.nums);
         //this prints out an empty list. write some code that will allow the data generated in the syncThread to show up  here.  There is a brute force way and a more sophisticated way.  Either or will work, but strive for sophistication :)
 
     }
-    
+
 }
--- src/lambda_streams/Numbers.java
+++ src/lambda_streams/Numbers.java
@@ -1,16 +1,22 @@
 package lambda_streams;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class Numbers {
-    static List<Integer> nums = Arrays.asList(10,100,1000,5,50,500,3,30,300,7,70,700,1,10,100,25,250,2500);
+    static List<Integer> nums = Arrays.asList(10, 100, 1000, 5, 50, 500, 3, 30, 300, 7, 70, 700, 1, 10, 100, 25, 250, 2500);
 
     public static void main(String[] args) {
         //Part I :complete the static class methods that have been set up in this Numbers class java file.  Use streams to compute the results wheever possible.
         System.out.println(nums);
 
+
         //Part II - refactor all of the class methods to accept lambda expressions. You can put the lambda functions directly inside the method calls, or defined them first, then pass them into the methods. give them the same names as the static methods, but add the word 'lambda' in front of every lambda function:
+
+        // lambadOdd = nums.forEach(Numbers::isOdd);
         /* e.g.
 
         added(() -> {});
@@ -32,60 +38,76 @@ class Numbers {
 
     }
 
+    private static List<Integer> lambaOdd(List<Integer> items) {
+        return items.stream().filter(Numbers::isOdd).collect(Collectors.toList());
+    }
+
     static boolean isOdd(int i) {
-        //determine if the value at the index i is odd.  return true if yes, return false if  no.
-        return false;
+        return nums.get(i) % 2 == 1;
+    }
+
+
+    private static List<Integer> lambaEven(List<Integer> items) {
+        return items.stream().filter(Numbers::isEven).collect(Collectors.toList());
     }
 
     static boolean isEven(int i) {
-        //determine if the value at the index i is even.  return true if yes, return false if  no.
-        return false;
+        return nums.get(i) % 2 == 0;
+    }
+
+
+    private static List<Integer> lambaPrime(List<Integer> items) {
+        return items.stream().filter(Numbers::isPrime).collect(Collectors.toList());
     }
 
     static boolean isPrime(int i) {
-         //determine if the value at the index i is a prime number.  return true if yes, return false if no.
-         return false;
+        if (i <= 1)
+            return false;
+        for (int j = 2; j < i; j++)
+            if (i % j == 0)
+                return false;
+        return true;
     }
 
-    static int added() {
-        //add all the elements in the list.  return the sum.  
-        return 0;
+    private static int lambaAdded(List<Integer> items) {
+        return items.stream().reduce(1, Integer::sum);
     }
 
-    static int subtracted() {
-        //subtract all the elements in the list. return the remainder.
-        return 0;
+    static int lambdasubtracted(List<Integer> items) {
+        return  nums.stream().reduce(nums.get(0), (a, b) -> a - b);
     }
 
-    static int multipled() {
-        //multiply all the elements in the list. and return the product.
-        return 0;
+    private static int lambdaMultiply(List<Integer> items) {
+        return items.stream().reduce(1, (a, b) -> a * b);
     }
 
-    static int divided() {
-        //multiply all the elements in the list. and return the product.
-        return 0;
+    static int lambdaDivided() {
+        return  nums.stream().reduce(nums.get(0), (a, b) -> a / b);
     }
 
-    static int findMax() {
-         //return the maximum value in the list.
-        return 0;
+    private static Integer lambdafindMax(List<Integer> items) {
+        return items.stream().max(Integer::compare).get();
     }
 
-    static int findMin() {
-        //return the minimum value in the list.
-        return 0;
+
+    private static Integer lambdafindMin(List<Integer> items) {
+        return items.stream().min(Integer::compare).get();
     }
 
     static int compare(int i, int j) {
-        //compare the values stored in the array at index position i and j.  
-        //if the value at i is greater, return 1.  if the value at j is greater, return -1.  if the two values are equal, return 0.
+        Stream<Integer> stream = Arrays.asList(nums.get(i), nums.get(j)).stream();
+        stream.forEach((Integer element) -> {
+        });
         return 0;
     }
 
+    private static void lambdaAddToList(List<Integer> items, int target) {
+        //items.forEach(items::target);
+    }
     static int append(int n) {
-        //add a new value to the values list. return that value after adding it to the list.
-        return 0;
+        List<Integer> addedNums = new ArrayList<Integer>(nums);
+        addedNums.add(n);
+        return n;
     }
 
 }
